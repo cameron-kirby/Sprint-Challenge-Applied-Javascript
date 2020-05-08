2065e150e610cb91a7faed22345e16097a9af586
Cameron Kirby <cameronevankirby@gmail.com>
2020-05-08 11:37:14 -0500

    Added MVP

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,19 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+let topics = document.querySelector('.topics');
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then(response => {
+        response.data.topics.forEach(function(item){
+            topics.appendChild(createTab(item))
+        })
+    })
+    .catch()
+
+function createTab(topic){
+    const tab = document.createElement('div');
+    tab.classList.add('tab');
+    tab.textContent = topic;
+    return tab;
+}
